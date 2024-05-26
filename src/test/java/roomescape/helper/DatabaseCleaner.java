4f55cbe59935089e0733069c44f9843ffc976daf
Choo <choo000407@naver.com>
2024-05-26 18:47:44 +0900

    clear table

--- src/test/java/roomescape/helper/DatabaseCleaner.java
+++ src/test/java/roomescape/helper/DatabaseCleaner.java
@@ -2,21 +2,66 @@ package roomescape.helper;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.Session;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @Transactional
-public class DatabaseCleaner {
+public class DatabaseCleaner implements InitializingBean {
     @PersistenceContext
     private EntityManager entityManager;
 
+    private List<String> tableNames;
+
     public void execute() {
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
+        clearTables();
         clearReservationWaiting();
         clearReservation();
         clearMember();
         clearTime();
         clearTheme();
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        entityManager.unwrap(Session.class).doWork(this::generateTableInformation);
+    }
+
+    private void generateTableInformation(Connection connection) throws SQLException {
+        List<String> tableNames = new ArrayList<>();
+        ResultSet tables = extractTables(connection);
+        while (tables.next()) {
+            String tableName = tables.getString("TABLE_NAME");
+            tableNames.add(tableName);
+        }
+        this.tableNames = tableNames;
+    }
+
+    private static ResultSet extractTables(Connection connection) throws SQLException {
+        return connection
+                .getMetaData()
+                .getTables(connection.getCatalog(), null, "%", new String[]{"TABLE"});
+    }
+
+    private void clearTables() {
+        for (String table : tableNames) {
+            System.out.println("table = " + table);
+        }
+//        tables.forEach(table -> {
+//            entityManager.createNativeQuery(String.format("DELETE FROM %s", table))
+//                    .executeUpdate();
+//            entityManager.createNativeQuery(String.format("ALTER TABLE %s ALTER COLUMN id RESTART", table))
+//                    .executeUpdate();
+//        });
     }
 
     private void clearReservationWaiting() {
